961ee3cc4ce816d0565badd2f875ddbe43e299f3
zhangwenchong <zhangwenchong@dataknown.cn>
2018-08-03 15:48:44 +0800

    修改demo首页的结构，方便之后的调试

--- bootstrap/demo/entry.jsx
+++ bootstrap/demo/entry.jsx
@@ -5,31 +5,43 @@ let ReactDOM = require("react-dom");
 let Alert = require("../src/components/alerts/index.jsx");
 let AlertLink = require("../src/components/alerts/link.jsx");
 let Badge = require("../src/components/badge/index.jsx");
-ReactDOM.render(
-    <div style={{ padding: '20px' }}>
-        <h1>这是最简单的实验页面</h1>
-        <div>
-            <div>
-                <h2>alerts</h2>
-                <Alert>
-                    This is a primary alert—check it out!
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div style={{ padding: '20px' }}>
+                <h1>这是最简单的实验页面</h1>
+                <div>
+                    <div>
+                        <h2>alerts</h2>
+                        <Alert>
+                            This is a primary alert—check it out!
                 </Alert>
-                <Alert theme="success" closeable>
-                    This is a primary alert—check it out!
+                        <Alert theme="success" closeable>
+                            This is a primary alert—check it out!
                 </Alert>
-                <Alert theme="info" closeable>
-                    This is a primary alert with <AlertLink>an example link</AlertLink>. Give it a click if you like.
+                        <Alert theme="info" closeable>
+                            This is a primary alert with <AlertLink>an example link</AlertLink>. Give it a click if you like.
                 </Alert>
-                <Alert heading="Well done!" closeable onClose={() => { alert(1) }}>
-                    This is a primary alert with <AlertLink>an example link</AlertLink>. Give it a click if you like.
+                        <Alert heading="Well done!" closeable onClose={() => { alert(1) }}>
+                            This is a primary alert with <AlertLink>an example link</AlertLink>. Give it a click if you like.
                 </Alert>
-                <h2>badges</h2>
-                <Badge theme="primary">primary</Badge>
-                <Badge theme="secondary" isPill>secondary</Badge>
-                <Badge theme="success">success</Badge>
-                <Badge theme="dark" href="###">dark</Badge>
+                        <h2>badges</h2>
+                        <Badge theme="primary">primary</Badge>
+                        <Badge theme="secondary" isPill>secondary</Badge>
+                        <Badge theme="success">success</Badge>
+                        <Badge theme="dark" href="###">dark</Badge>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>,
+        )
+    }
+}
+
+ReactDOM.render(
+    <Root />,
     document.getElementById(ROOT_CONTAINER)
 );
